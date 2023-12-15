783c1eac70cc57f2336b206b58c6c531361561dd
Mehmet Kaba <gm.mkaba@gmail.com>
2023-12-15 22:45:29 +0300

    Javascript özellikleri

---
+++ ArrowFunctions/arrowFunctions.js
@@ -0,0 +1,8 @@
+
+// JavaScript Implicit Returns (örtülü return)
+const square = (x) => x * x;
+
+const add = (n1, n2) => n1 + n2;
+
+console.log(square(5));
+console.log(add(3, 4));
---
+++ ShorthandPropertyNames/shorthandPropertyNames.js
@@ -0,0 +1,18 @@
+// Shorthand Property Names özelliği olmadan.
+const name = "Jack";
+const surname = "London";
+const authorInfo = {authorName: name, authorSurname: surname};
+
+console.log(authorInfo);
+
+
+
+
+
+// Shorthand Property Names özelliğiyle
+const productName = "Laptop";
+const productPrice = 10000;
+const productColor = "Gray";
+const productInfo = {productName, productPrice, productColor};
+
+console.log(productInfo);
